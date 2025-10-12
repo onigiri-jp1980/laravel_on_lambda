9b9d0d04d1dad43d8896f47594f626e22ee93c22
onigiri-jp1980 <d.yamashiro.mob@gmail.com>
2025-10-12 11:51:29 +0900

    サンプルモデル追加

--- app/config/app.php
+++ app/config/app.php
@@ -122,8 +122,6 @@ return [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
-    'providers' => [
-        BaoPham\DynamoDb\DynamoDbServiceProvider::class,
-    ],
+    
 
 ];
